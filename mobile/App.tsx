6cfc5957f1d9b43db416dd681a83410bb29fa9ff
Victor <victorabiscula@live.com>
2022-05-06 00:35:16 -0300

    importado biblioteca de gestos

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,11 @@
+import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { theme } from './src/theme';
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,13 +22,13 @@ export default function App() {
       flex: 1,
       backgroundColor: theme.colors.background
     }}>
-      < Widget />
-
       <StatusBar 
         style="light"
         backgroundColor="transparent"
         translucent
       />
+
+      < Widget />
     </View>
   )
 }
