28fdde02b061268536a50f5ce066c19561059a76
Victor <victorabiscula@live.com>
2022-05-05 22:40:09 -0300

    reestruturação do arquivo de rotas

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,35 +1,27 @@
 import express from 'express'
-import nodemailer from 'nodemailer'
-import { prisma } from './prisma'
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router()
-
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "06d8c01282825c",
-      pass: "6bb4430dda9ef9"
-    }
-});
 
 routes.post('/feedbacks', async (req, res) => {
 
     const { type, comment, screenshot } = req.body
     
-    const feedback = 
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+    const nodemailerMailAdapter = new NodemailerMailAdapter()
 
-    await transport.sendMail({
-        from: 'Equipe Feedget <oi@feedget.com>',
-        to: 'Victor Abiscula <victorabiscula@live.com>',
-        subject: 'Novo feedback',
-        html: [
-            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-                `<p>Tipo do feedback: ${type}</p>`,
-                `<p>Comentário: ${comment}</p>`,
-            `</div>`
-        ].join('\n')
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+        prismaFeedbacksRepository,
+        nodemailerMailAdapter
+    )
+
+    await submitFeedbackUseCase.execute({
+        type: type,
+        comment: comment,
+        screenshot: screenshot
     })
 
-    return res.status(201).json({data: feedback})
+    return res.status(201).send()
 })
