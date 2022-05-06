092ecc3978fcb5112cf8e916a5d399b15c5cdcbe
Victor <victorabiscula@live.com>
2022-05-05 22:38:16 -0300

    estruturado casos de uso
    implementado testes para validação das funções

--- back-end/src/use-cases/submit-feedback-use-case.ts
+++ back-end/src/use-cases/submit-feedback-use-case.ts
@@ -1,3 +1,4 @@
+import { MailAdapter } from '../adapters/mail-adapter';
 import { FeedbacksRepository } from '../repositories/feedbacks-repository'
 
 interface SubmitFeedbackUseCaseRequest {
@@ -8,15 +9,40 @@ interface SubmitFeedbackUseCaseRequest {
 
 export class SubmitFeedbackUseCase {
 
-    constructor(private feedbacksRepository: FeedbacksRepository) {}      
+    constructor(
+        private feedbacksRepository: FeedbacksRepository,
+        private mailAdapter: MailAdapter
+    ) {}      
     
     async execute(request: SubmitFeedbackUseCaseRequest) {
         const { type, comment, screenshot } = request
+
+        if(!type) {
+            throw new Error('Type is required')
+        }
+
+        if(!comment) {
+            throw new Error('Comment is required')
+        }
+
+        if(screenshot && !screenshot.startsWith('data:image/png;base64')) {
+            throw new Error('Invalid screenshot format.')
+        }
 
         await this.feedbacksRepository.create({
             type: type,
             comment: comment,
             screenshot: screenshot
+        })
+
+        await this.mailAdapter.sendMail({
+            subject: 'Novo feedback',
+            body: [
+                `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+                    `<p>Tipo do feedback: ${type}</p>`,
+                    `<p>Comentário: ${comment}</p>`,
+                `</div>`
+            ].join('\n')
         })
     }
 }
