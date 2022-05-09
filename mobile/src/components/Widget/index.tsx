51daab0a3da512a214382b2c6f688dc67b61f31a
Victor <victorabiscula@live.com>
2022-05-08 22:10:02 -0300

    criado funções para validar o envio do feedback

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native'
 import BottomSheet from '@gorhom/bottom-sheet';
@@ -16,10 +16,22 @@ export type FeedbackType = keyof typeof feedbackTypes
 
 function Widget() {
 
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false)
+
     const bottomSheetRef =  useRef<BottomSheet>(null)
 
     function handleOpen() {
         bottomSheetRef.current?.expand()
+    }
+
+    function handleRestartFeedback() {
+        setFeedbackType(null)
+        setFeedbackSent(false)
+    }
+
+    function handleFeedbackSent() {
+        setFeedbackSent(true)
     }
 
     return ( 
@@ -41,9 +53,24 @@ function Widget() {
                 backgroundStyle={styles.modal}
                 handleIndicatorStyle={styles.indicator}
             >
-                <Form 
-                    feedbackType="BUG"
-                />
+                {
+                    feedbackSent 
+                    ?   <Success 
+                            onSendAnotherFeedback={handleRestartFeedback}
+                        />
+                    :
+                    <>
+                        {
+                            feedbackType 
+                            ?   <Form 
+                                    feedbackType={feedbackType} 
+                                    onFeedbackCanceled={handleRestartFeedback}
+                                    onFeedbackSent={handleFeedbackSent}
+                                />
+                            :   <Options onFeedbackTypeChanged={setFeedbackType}/>
+                        }
+                    </>
+                }
             </BottomSheet>
         </>
     );
