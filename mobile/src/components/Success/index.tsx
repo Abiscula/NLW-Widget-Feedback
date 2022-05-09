ba8eb39e79c9b34b8935591e660258e54a50643f
Victor <victorabiscula@live.com>
2022-05-08 22:09:31 -0300

    criado tipagem e passado estado via props

--- mobile/src/components/Success/index.tsx
+++ mobile/src/components/Success/index.tsx
@@ -5,7 +5,11 @@ import successImg from '../../assets/success.png'
 import { Copyright } from '../Copyright';
 import { styles } from './styles';
 
-export function Success() {
+interface Props {
+    onSendAnotherFeedback: () => void;
+}
+
+export function Success({ onSendAnotherFeedback }: Props) {
   return (
     <View style={styles.container}>
         <Image 
@@ -17,7 +21,10 @@ export function Success() {
             Agradecemos o Feedback
         </Text>
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity 
+            style={styles.button}
+            onPress={onSendAnotherFeedback}
+        >
             <Text style={styles.buttonTitle}>
                 Quero enviar outro
             </Text>
